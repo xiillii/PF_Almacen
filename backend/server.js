36cded3cc787a2f3dc63cb3c9acfda4f6428e25f
Jose Alonso <mail@josealonso.dev>
2022-03-27 03:40:22 -0600

    Configuración inicial para la conexión a MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import colors from 'colors';
 
 const app = express();
 
 dotenv.config();
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
