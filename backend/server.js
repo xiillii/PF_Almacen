b8a045d8c3aa6856f255a32c52b2f12addc1266e
Jose Alonso <mail@josealonso.dev>
2022-03-27 03:18:26 -0600

    Configuración inicial del proyecto MERN. Frontend básico y configuración inicial del backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
