f89b08c17544dc7684013f5a490c38b6f9ca7806
Jose Alonso <mail@josealonso.dev>
2022-04-27 17:30:27 -0500

    RF28. Inicial de Kardex controller

---
+++ backend/controllers/kardexController.js
@@ -0,0 +1,6 @@
+import asyncHandler from 'express-async-handler';
+import Kardex from '../models/kardexModel.js';
+import Warehouse from '../models/warehouseModel.js';
+import Product from '../models/productModel.js';
+import MovementType from '../models/movementTypeModel.js';
+import Stock from '../models/stockModel.js';
